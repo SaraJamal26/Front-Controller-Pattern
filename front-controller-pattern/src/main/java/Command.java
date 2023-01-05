84d4002fb2f564ba00ce41b7c0949709c1dc33a6
SaraJamal26 <sara2001jamal@gmail.com>
2023-01-06 04:48:36 +0500

    Revert "Initial commit"
    This reverts commit fd0833f10f0330f4b45644734bf245c30b545c4a.

--- front-controller-pattern/src/main/java/Command.java
+++
@@ -1,7 +0,0 @@
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface Command {
-	String execute(HttpServletRequest request, HttpServletResponse response);
-
-}
